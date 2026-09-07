931436853360448716066fd3d2e800cac0c5079f
Colin Dugeai <colin.dugeai@gmail.com>
2026-09-07 20:08:27 +0200

    feat: can click pins

--- src/lib/components/map/MapLayerSchool.svelte
+++ src/lib/components/map/MapLayerSchool.svelte
@@ -1,43 +1,44 @@
 <script lang="ts">
   import 'svelte-maplibre-gl/vite'; // Required only for GL JS v6+
-  import { GeoJSONSource, CircleLayer } from 'svelte-maplibre-gl';
+  import { Marker, Popup } from 'svelte-maplibre-gl';
   import type { School } from '$lib/types/school';
+  import type * as maplibregl from 'maplibre-gl';
 
   interface Props {
     schools: School[];
   }
 
   const { schools }: Props = $props();
-  let geoJsonData = $derived({
-    type: 'FeatureCollection',
-    features: schools.map((school) => ({
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [school.longitude, school.latitude],
-      },
-      properties: {
-        id: school.id,
-        name: school.name,
-        address: school.address,
-        city: school.city,
-        postalCode: school.postalCode,
-      },
-    })),
+
+  let offset = $state(24);
+
+  let offsets: maplibregl.Offset = $derived({
+    top: [0, offset],
+    bottom: [0, -offset],
+    left: [offset + 12, 0],
+    right: [-offset - 12, 0],
+    center: [0, 0],
+    'top-left': [offset, offset],
+    'top-right': [-offset, offset],
+    'bottom-left': [offset, -offset],
+    'bottom-right': [-offset, -offset],
   });
+  let selected_school: string = $state('');
 </script>
 
-{#if geoJsonData.features.length > 0}
-  <GeoJSONSource id="schools" data={geoJsonData} />
-  <CircleLayer
-    id="schools-layer"
-    source="schools"
-    paint={{
-      'circle-radius': 8,
-      'circle-color': '#3b82f6',
-      'circle-stroke-width': 2,
-      'circle-stroke-color': '#fff',
-      'circle-opacity': 0.8,
-    }}
-  />
-{/if}
+{#each schools as school (school.id)}
+  {@const lnglat = { lng: school.longitude, lat: school.latitude }}
+  {@const is_selected = school.id == selected_school}
+  <Marker {lnglat} onclick={() => selected_school == school.id}>
+    {#snippet content()}
+      <div class="text-center leading-none">
+        <div class="text-3xl">📍</div>
+        <!-- Text under icon -->
+        <!-- <div class="font-bold text-black drop-shadow-xs">{school.name}</div> -->
+      </div>
+    {/snippet}
+    <Popup class="text-black" open={is_selected} offset={offsets}>
+      <span class="text-sm">{school.name}</span>
+    </Popup>
+  </Marker>
+{/each}
