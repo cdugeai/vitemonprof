9ca38992e9f1cdc945440c1b8b7c9d3e977e6766
Colin Dugeai <colin.dugeai@gmail.com>
2026-09-07 20:08:27 +0200

    feat: add presenter school

---
+++ src/lib/components/form_class/PresenterSchool.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { School } from '$lib/types/school';
+
+  let {
+    school,
+  }: {
+    school: School | null;
+  } = $props();
+</script>
+
+<div class="flex flex-col">
+  {#if school == null}
+    <p>No school selected</p>
+  {:else}
+    <p>{school.name}</p>
+    <div class="flex flex-wrap gap-x-3 gap-y-0">
+      <p>{school.city} ({school.postalCode})</p>
+      <p class="font-light text-gray-500">{school.address}</p>
+    </div>
+  {/if}
+</div>
